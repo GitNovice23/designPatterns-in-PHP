16f33aa835e612d3f0984672133d2e165b2a673e
GitNovice23 <brandonambe29@gmail.com>
2026-01-16 08:42:22 +0100

    Facade design pattern

---
+++ facade/facade.php
@@ -0,0 +1,17 @@
+class CPU {
+    public function start() {
+        echo "CPU started";
+    }
+}
+
+class Memory {
+    public function load() {
+        echo "Memory charged";
+    }
+}
+
+class HardDrive {
+    public function load() {
+        echo "hardDisk reading";
+    }
+}
